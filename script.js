91b117af644f49174f5fd0d6ed1d60fdb9e05195
springsc123 <springscullen@yahoo.com>
2019-10-23 06:26:09 +0000

    vue setup, just need to hook in the API

--- script.js
+++ script.js
@@ -2,9 +2,27 @@
 let app = new Vue({
     el: '#app',
     data: {
+        values: {
+            studentName: ' ',
+            studentNumber: '',
+            reviewedNumber: '', 
+            creativityScore: '',
+            wowScore: '',
+            comments: ' '
+        }
     },
     methods: {
-        
+        async submitForm() {
+            try {
+                const response = await axios.get('https://xkcdapi.now.sh/' + this.number);
+                this.current = response.data;
+                this.loading = false;
+                this.number = response.data.num;
+            }
+            catch (error) {
+                console.log(error);
+            }
+        }
 
     },
     computed: {
