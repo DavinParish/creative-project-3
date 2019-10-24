3a79e85fa2b3d2a47337047b32e8cee30d647c26
springsc123 <springscullen@yahoo.com>
2019-10-24 19:59:05 +0000

    updated url

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
 let app = new Vue({
     el: '#app',
     data: {
+        currRange : '2',
         values: {
             studentName: ' ',
             studentNumber: '',
-            reviewedNumber: '', 
+            reviewedNumber: '',
             creativityScore: '',
             wowScore: '',
             comments: ' '
@@ -14,21 +15,19 @@ let app = new Vue({
     methods: {
         async submitForm() {
             try {
-                const response = await axios.get('https://xkcdapi.now.sh/' + this.number);
-                this.current = response.data;
-                this.loading = false;
-                this.number = response.data.num;
+                /* global axios*/
+                const spreadsheetId = "1XS4E90X96Xha5E-8EypzasCaZFgrYlB7k5BETygKgZw";
+                const range = this.currRange + "1:" + this.currRange + "" ;
+                const url = "https://sheets.googleapis.com/v4/spreadsheets/" + spreadsheetId + "/values/" + range;
+                const response = await axios.post(url);
+                console.log(response)
+                this.currRange = this.currRange + 1
             }
             catch (error) {
                 console.log(error);
+                console.log("WE had a problem");
             }
-        }
+        },
 
-    },
-    computed: {
-       
-    },
-    watch: {
-        
-    },
+    }
 });
